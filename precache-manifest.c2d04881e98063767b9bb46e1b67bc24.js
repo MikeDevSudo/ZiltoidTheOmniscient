4bb9d7dd663846e2380bb558e780d75d5d8b2f7f
mike <oddball_mjd@hotmail.co.uk>
2023-07-28 15:08:34 +0400

    Updates

---
+++ precache-manifest.c2d04881e98063767b9bb46e1b67bc24.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae90c89ab0c5efe96773",
+    "url": "/ZiltoidTheOmniscient/static/css/main.91cd51a7.chunk.css"
+  },
+  {
+    "revision": "ae90c89ab0c5efe96773",
+    "url": "/ZiltoidTheOmniscient/static/js/main.ae90c89a.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/ZiltoidTheOmniscient/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "16e7e167663f7aa68119",
+    "url": "/ZiltoidTheOmniscient/static/js/2.16e7e167.chunk.js"
+  },
+  {
+    "revision": "e65d45fa32cb42919fe1",
+    "url": "/ZiltoidTheOmniscient/static/js/runtime~main.e65d45fa.js"
+  },
+  {
+    "revision": "1f6f91143e90db60dd14a6ff84127a8d",
+    "url": "/ZiltoidTheOmniscient/static/media/Launch.1f6f9114.png"
+  },
+  {
+    "revision": "b04411824a4101b102279d67107ffe0f",
+    "url": "/ZiltoidTheOmniscient/static/media/CoffeeMug.b0441182.png"
+  },
+  {
+    "revision": "97932d3adcedafdce1201b360b9a94c6",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-0.97932d3a.png"
+  },
+  {
+    "revision": "daf9b52aaad2094b6528262cfc9b0ee4",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-1.daf9b52a.png"
+  },
+  {
+    "revision": "5671759c1f4d9796ca6554e40c60d331",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-2.5671759c.png"
+  },
+  {
+    "revision": "d5d4fb7e5b79ba48baaf036e0e5856e5",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-3.d5d4fb7e.png"
+  },
+  {
+    "revision": "0070dc5eed7c4b88f1813d108b78cce9",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-4.0070dc5e.png"
+  },
+  {
+    "revision": "ed0f809e8c9db2d4e27e4036dc10341c",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-5.ed0f809e.png"
+  },
+  {
+    "revision": "c9e5228c31b5ced8c40459456d038eb4",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-6.c9e5228c.png"
+  },
+  {
+    "revision": "223c74b53797115672f04d5996812dc7",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-7.223c74b5.png"
+  },
+  {
+    "revision": "f720592dea990ca38f97c564bef30686",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-8.f720592d.png"
+  },
+  {
+    "revision": "e40a8590af681e9ca67377143aa682bb",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-lose.e40a8590.png"
+  },
+  {
+    "revision": "b3c26c4b02bde2d4c381cc5ce4b86bb1",
+    "url": "/ZiltoidTheOmniscient/static/media/Progress-win.b3c26c4b.png"
+  },
+  {
+    "revision": "417b9dda085e335af4ebe98cef612c8f",
+    "url": "/ZiltoidTheOmniscient/index.html"
+  }
+];
